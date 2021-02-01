d2c5675131164423e84bba9385348b3be0c5606a
Marcin Bugaj <33054342+schmitshny@users.noreply.github.com>
2021-02-01 13:22:46 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,50 @@
+//zadanie 1
+
+let x = true;
+let y = true;
+
+console.log(x==y);
+
+//zadanie 2
+
+let x=10;
+let y=3;
+moduloResult = x%y;
+
+console.log(moduloResult);
+
+//zadanie 3
+
+let x = "Java";
+let y = "script";
+let stringsResult = "";
+stringsResult = x+y;
+console.log(stringsResult);
+
+//zadanie 4
+
+var someNumber=425;
+var someString="425";
+
+console.log(someNumber==someString);
+console.log(someNumber===someString);
+
+// === Zwraca true jeżeli operandy są równe i tego samego typu a
+// == Zwraca true jeżeli operandy są równe. Jeżeli operandy nie są tego samego typu, JavaScript próbuje przekształcić operandy na odpowiedni dla porównania typ.
+
+// zadanie 5
+
+let counter = 30;
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+
+// zadanie 6
+
+let x=5, y=6;
+let result = null;
+result = x>y;
+console.log(result);
