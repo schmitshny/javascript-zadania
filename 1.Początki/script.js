562debcf4374805bd4d762d2ea8ed8cac0f11e17
Marcin Bugaj <33054342+schmitshny@users.noreply.github.com>
2021-02-01 12:57:33 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+// zadanie 1
+
+console.log("Marcin");
+
+// zadanie 2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+//zadanie 3
+
+console.log(("Hello World");
+console.log((Hello World);
+console.log("Hello World);
