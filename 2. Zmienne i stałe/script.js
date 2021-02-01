4a1543b2b80e7fc5fa2880b60eb27e04f0f452ad
Marcin Bugaj <33054342+schmitshny@users.noreply.github.com>
2021-02-01 13:07:50 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,29 @@
+// zadanie 1 
+
+let number=2;
+let string ="Marcin";
+let numstring= 2+"Marcin";
+let bool = false;
+let x = null;
+
+console.log(number);
+console.log(string);
+console.log(numstring);
+console.log(bool);
+console.log(x);
+
+//zadanie 2
+
+let x=10;
+let y=15;
+sum=x+y;
+
+console.log(sum);
+
+//zadanie 3
+
+let x;
+
+console.log(x);
+
+//output noValue bo nie przypisaliśmy wartośći do x
